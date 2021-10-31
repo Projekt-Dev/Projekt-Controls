c2ea618af029d4ae402f9c126e288bec9b15d1f8
marqu <marqu@SCARLET>
2021-10-30 17:48:21 -0700

    Add project files.

---
+++ Core/Components/ProjektDragComponent.vb
@@ -0,0 +1,34 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.InteropServices
+
+Public Class ProjektDragComponent
+    Inherits Component
+
+    Private handleControl As Control
+
+    Public Property SelectedControl As Control
+        Get
+            Return Me.handleControl
+        End Get
+        Set(value As Control)
+            Me.handleControl = value
+            AddHandler Me.handleControl.MouseDown, New MouseEventHandler(AddressOf Me.Mouse_Down)
+        End Set
+    End Property
+    <DllImport("user32.dll")>
+    Public Shared Function SendMessage(a As IntPtr, msg As Integer, wParam As Integer, lParam As Integer) As Integer
+
+    End Function
+    <DllImport("user32.dll")>
+    Public Shared Function ReleaseCapture() As Boolean
+
+    End Function
+    Private Sub Mouse_Down(sender As Object, e As MouseEventArgs)
+        Dim flag As Boolean = e.Button = MouseButtons.Left
+        If flag Then
+            ProjektDragComponent.ReleaseCapture()
+            ProjektDragComponent.SendMessage(Me.SelectedControl.FindForm().Handle, 161, 2, 0)
+        End If
+    End Sub
+
+End Class
---
+++ Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.pnlHeader = New System.Windows.Forms.Panel()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.ProjektDragComponent1 = New Projekt_Controls.ProjektDragComponent()
+        Me.pnlHeader.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlHeader
+        '
+        Me.pnlHeader.BackColor = System.Drawing.Color.DarkRed
+        Me.pnlHeader.Controls.Add(Me.btnExit)
+        Me.pnlHeader.Controls.Add(Me.lblTitle)
+        Me.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlHeader.Location = New System.Drawing.Point(0, 0)
+        Me.pnlHeader.Name = "pnlHeader"
+        Me.pnlHeader.Size = New System.Drawing.Size(1290, 46)
+        Me.pnlHeader.TabIndex = 0
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Transparent
+        Me.btnExit.Dock = System.Windows.Forms.DockStyle.Right
+        Me.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btnExit.FlatAppearance.BorderSize = 2
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Font = New System.Drawing.Font("Montserrat SemiBold", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(1215, 0)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 46)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "X"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblTitle.Font = New System.Drawing.Font("Montserrat Medium", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.White
+        Me.lblTitle.Location = New System.Drawing.Point(464, 9)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(362, 29)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "PROJEKT CUSTOM CONTROLS"
+        '
+        'ProjektDragComponent1
+        '
+        Me.ProjektDragComponent1.SelectedControl = Me.pnlHeader
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1290, 655)
+        Me.Controls.Add(Me.pnlHeader)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.pnlHeader.ResumeLayout(False)
+        Me.pnlHeader.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlHeader As Panel
+    Friend WithEvents ProjektDragComponent1 As ProjektDragComponent
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents btnExit As Button
+End Class
